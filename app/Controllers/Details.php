90acd2ac80eefcbca3a3655bd0d6ae06fefe3a10
Ariyana <darkangel1263@hotmail.com>
2022-04-06 20:33:15 -0400

    Implemetation of front end for cart, functionality for user to be logged in and remove from cart.

--- app/Controllers/Details.php
+++ app/Controllers/Details.php
@@ -4,17 +4,28 @@
 require_once APP_DIR . "Config/Database.php";
 require_once APP_DIR . "Models/User.php";
 require_once APP_DIR . "Models/Product.php";
+require_once APP_DIR . "Models/Cart.php";
+
 
 // create objects
 $db_object = new Database();
 $user_object = new User($db_object);
 $product_object = new Product($db_object);
+$cart_object = new Cart($db_object);
 
 $product_details = $product_object->getProductDetails($id);
 //debug($product_details);
 
 
-debug($_POST);
+//debug($_POST);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["cart_quantity"])) {
+        echo "you clicked a button";
+        require_once APP_DIR . "utils/code.isLoggedIn.php";
+        $cart_object->addToCart($user_id, $id, $_POST["cart_quantity"]);
+    }
+}
 
 foreach ($product_details as $data) {
     # code...
