183a67c7796ba844c30e3916596ca9c1ab7a2949
Ariyana <darkangel1263@hotmail.com>
2022-04-06 20:32:27 -0400

    Implementation of cart front-end with user logged in to view cart and remove button functionality

--- app/Controllers/Cart.php
+++ app/Controllers/Cart.php
@@ -4,6 +4,7 @@
 require_once APP_DIR . "Config/Database.php";
 require_once APP_DIR . "Models/User.php";
 require_once APP_DIR . "Models/Cart.php";
+require_once APP_DIR . "utils/code.isLoggedIn.php";
 
 // create objects
 $db_object = new Database();
@@ -14,8 +15,14 @@ $cart_object = new Cart($db_object);
 //$cart_object->addToCart(2,2,2,);
 //cart_object->removeFromCart(1,1);
 
-$cart_details = $cart_object->getCartDetails(1);
-debug($cart_details);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["cart_id"])) {
+        $cart_object->removeFromCart($_POST["cart_id"], $user_id);
+    }
+}
+
+$cart_details = $cart_object->getCartDetails($user_id);
+//debug($cart_details);
 
 // load views
 require_once APP_DIR . "Views/header.php";
